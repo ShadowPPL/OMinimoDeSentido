747828d22935939d478f0afd1ba0396077bcf04b
Held Grijo <shadyueh@gmail.com>
2018-11-11 11:54:14 -0400

    Initial project

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Copy third party libraries from /node_modules into /vendor
+gulp.task('vendor', function () {
+  // Bootstrap
+  gulp.src([
+      './node_modules/bootstrap/dist/**/*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+    ])
+    .pipe(gulp.dest('./vendor/bootstrap'))
+
+  // jQuery
+  gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'))
+
+});
+
+// Configure the browserSync task
+gulp.task('browserSync', function () {
+  browserSync.init({
+      server: {
+          baseDir: "./",
+      },
+      port: 8080,
+  })
+});
+
+gulp.task('watch',function () {
+  gulp.watch(['./css/*.css'], browserSync.reload);
+  gulp.watch(['./**/*.html'], browserSync.reload);
+});
+
+// Dev task
+gulp.task('dev', gulp.parallel('browserSync', 'watch'));
+
+// Default task
+gulp.task('default',  gulp.series('vendor','dev'));
